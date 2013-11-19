87113243ec535fa8b08548d71d8ce9d583dd505e
Ben <bencollins2@gmail.com>
2013-11-19 12:05:18 -0500

    Some register stuff

--- register.php
+++ register.php
@@ -101,61 +101,15 @@ else{
   }
   ?>
 </div>
-<div class="bg two"></div>
-<div class="bg one"/></div>
-<div class="semitrans"></div>
 <div id='register'>
-    <div class="left">
-      <div class="vfm">Victors for <span>Michigan</span></div>
-              <h2>Sign Up and <br />Get Started</h2>
+  <div class="ve">Victors Experience</div>
+  <a class="cta">Sign Up Now</a>
+  <p class="reg">
+    Customize this website to filter through the amazing work being done by Michigan Engineers, who are stepping up to tackle the world's greatest problems.
+  </p>
 
-      <a class="fbl" href="#">
-        <?= ($linked) ? "<!--h2>We've selected some content we think you'll like.</h2-->" : ""?>
-        <img src="img/signupwithfb_220.png" />
-      </a>
-      <div class="or">
-        <span>..or if you'd prefer, you can <a href="#" class="create">create an account directly</a>.</span>
-      </div>
-      <div class="createacct">
-        <form action="process.php" method="POST">
 
-          <div class="table">
-            <div><span>Email:</span><span><input type="text" name="email" maxlength="50" value="<? echo $form->value("email"); ?>"></span><span><? echo $form->error("email"); ?></span></div>
-            <div><span>Password:</span><span><input type="password" name="pass" maxlength="30" value="<? echo $form->value("pass"); ?>"></span><span><? echo $form->error("pass"); ?></span></div>
-            <div><span>First Name:</span><span><input type="text" name="first" maxlength="30" value="<? echo $form->value("first"); ?>"></span><span><? echo $form->error("first"); ?></span></div>
-            <div><span>Last Name:</span><span><input type="text" name="last" maxlength="30" value="<? echo $form->value("last"); ?>"></span><span><? echo $form->error("last"); ?></span></div>
-            <div class="submit"><span colspan="2" align="right">
-            <input type="hidden" name="subjoin" value="1">
-            <?php if ($_GET["link"] > 0) { 
-              $lnk = mysql_real_escape_string($_GET["link"]);
-            ?> 
-            <input type="hidden" name="link" value="<?= $lnk?>">
-            <? }?>
-            <input type="submit" value="Join!"></span></div>
-          </div>
-        </form>
-      </div>
-      <div class="info">
-
-        <?php if ($_GET["link"] > 0) { 
-              $lnk = mysql_real_escape_string($_GET["link"]);
-            ?> 
-        <p>This customized "Victors Experience" website will allow you to filter through all the amazing work being done by Michigan Engineers on campus and around the world. Use the navigation above to get started.</p>
-            <? } else { ?>
-
-        <p>This customizable "Victors Experience" website will allow you to filter through all the amazing work being done by Michigan Engineers on campus and around the world. Use the navigation above to get started and create your own custom experience.</p>
-        <? }?>
-      </div>
-
-    </div>
-    <div class="right">
-      <p>The world is facing challenges, the likes of which it has never faced before. Problems that only engineers can solve.</p> 
-      <p>Because we are Michigan Engineering, </p>
-      <p>we are stepping up</p>
-      <p>to tackle these great challenges and make breakthrough solutions. </p>
-      <p class="play"><a href="http://www.youtube.com/embed/g5J9tcRuX9U?rel=0&amp;wmode=transparent" class="youtube cboxElement" alt="Watch the video"><img src="./img/play_80.png" />Watch the video</a></p>
-      <p class="learnmore"><a href="http://victors.engin.umich.edu/article.php?id=115" class="" alt="Why be a victor"><img src="./img/qmark_80.png" />Learn more about the campaign</a></p>
-    </div>
+  </div>
   <?
   }
   ?>
