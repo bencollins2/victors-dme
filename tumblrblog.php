9b3bf833724eba7a6fbfea072e8b1f77d9d68085
leonatefm <chengqi@umich.edu>
2013-11-22 17:17:57 -0500

    tumblr slice and blogs fetching in server side

---
+++ tumblrblog.php
@@ -0,0 +1,22 @@
+<?php
+
+require 'vendor/autoload.php';
+
+// Authenticate via OAuth
+$client = new Tumblr\API\Client(
+  'bd8RFLG5vt8pP31SZruALVSE6eFWHOTVPkeUpzh3gfADwcM8I3',
+  'INIbiXN9J4r0c8qSEjVDjzMI68crnYhc7bf6VyYxI34BfL7nlW',
+  'ws7yh5tPwLvt1xJ2VNkqFXGj2wgb6rqI5hEGRqRczWUgvhzqE3',
+  'SJZuuf9muRHkKL1YYctgGLmNBggrPY8RBNKy2v3uSs5t20uGxI'
+);
+
+// Make the request
+
+$blogName = "umichenginvictors";
+$blogs = $client->getBlogPosts($blogName, $options = null);
+
+?>
+
+<pre>
+	<?php print_r($blogs);?> 
+</pre>
