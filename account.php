1500d59a26d152e358ed1a4f60f11bde745c8e52
leonatefm <chengqizhufm@gmail.com>
2014-01-27 17:11:52 -0500

    add account information tab page

--- account.php
+++ account.php
@@ -316,119 +316,134 @@
 		</div>
 		
 		<div class="right">
+			<div class="tabs">
+				<ul>
+					<li class="cattab selected">Categories</li><li class="acctab">Account Info</li>
+				</ul>
+			</div>
 	        <div class="quiz" style="display:block">
-	        	<h2>Select some things that you like.</h2>
-	        	<ul>
-	        		<li class="transportation">
-	                    <div class="imgdiv">
-	                        <img src="img/cats/transportation.jpg" />
-	                    </div>
-	                    <div class="overlay">
-	                        <h3>Transportation Innovations</h3>
-	                    </div>
-	                    <div class="checkboxes">
-	                        <div data-shortname="vautonomous" class="item"><img src="img/box.png" alt="checkbox" /><span>Autonomous Vehicles</span></div>
-	                        <div data-shortname="invehicle" class="item"><img src="img/box.png" alt="checkbox" /><span>In-Vehicle Technology</span></div>
-	                        <div data-shortname="vefficiency" class="item"><img src="img/box.png" alt="checkbox" /><span>Vehicle Efficiency</span></div>
-	                        <div data-shortname="vsafety" class="item"><img src="img/box.png" alt="checkbox" /><span>Vehicle Safety</span></div>
-	                        <div data-shortname="vpowersources" class="item"><img src="img/box.png" alt="checkbox" /><span>Power Sources</span></div>
-	                    </div>
-	                </li>
-	                <li class="economics">
-	                    <div class="imgdiv">
-	                        <img src="img/cats/economics.jpg" />
-	                    </div>
-	                    <div class="overlay">
-	                        <h3>Economics & Entrepreneurship</h3>
-	                    </div>
-	                    <div class="checkboxes">
-	                        <div data-shortname="startups" class="item"><img src="img/box.png" alt="checkbox" /><span>Startups</span></div>
-	                        <div data-shortname="highrisk" class="item"><img src="img/box.png" alt="checkbox" /><span>High-Risk Research</span></div>
-	                        <div data-shortname="entreco" class="item"><img src="img/box.png" alt="checkbox" /><span>Entrepreneurial Ecosystem</span></div>
-	                        <div data-shortname="ecoimpact" class="item"><img src="img/box.png" alt="checkbox" /><span>Economic Impact</span></div>
-	                    </div>
-	                </li>
-	                <li class="wolverineexperience">
-	                    <div class="imgdiv">
-	                        <img src="img/cats/wolverineexperience.jpg" />
-	                    </div>
-	                    <div class="overlay">
-	                        <h3>Wolverine Experience</h3>
-	                    </div>
-	                    <div class="checkboxes">
-	                        <div data-shortname="handson" class="item"><img src="img/box.png" alt="checkbox" /><span>Hands-On Engineering</span></div>
-	                        <div data-shortname="leadersbest" class="item"><img src="img/box.png" alt="checkbox" /><span>Academic Leaders & Best</span></div>
-	                        <div data-shortname="world" class="item"><img src="img/box.png" alt="checkbox" /><span>Wolverines Around the World</span></div>
-	                        <div data-shortname="innovations" class="item"><img src="img/box.png" alt="checkbox" /><span>Innovations at Michigan</span></div>
-	                        <div data-shortname="stories" class="item"><img src="img/box.png" alt="checkbox" /><span>Victor Stories</span></div>
-	                        <div data-shortname="life" class="item"><img src="img/box.png" alt="checkbox" /><span>Life in Ann Arbor</span></div>
-	                    </div>
-	                </li>
-	                <li class="globalresources">
-	                    <div class="imgdiv">
-	                        <img src="img/cats/globalresources.jpg" />
-	                    </div>
-	                    <div class="overlay">
-	                        <h3>Global Resources</h3>
-	                    </div>
-	                    <div class="checkboxes">
-	                        <div data-shortname="water" class="item"><img src="img/box.png" alt="checkbox" /><span>Water</span></div>
-	                        <div data-shortname="sustain" class="item"><img src="img/box.png" alt="checkbox" /><span>Sustainability</span></div>
-	                        <div data-shortname="eefficiency" class="item"><img src="img/box.png" alt="checkbox" /><span>Energy Efficiency</span></div>
-	                        <div data-shortname="alternativee" class="item"><img src="img/box.png" alt="checkbox" /><span>Alternative Energy</span></div>
-	                        <div data-shortname="environment" class="item"><img src="img/box.png" alt="checkbox" /><span>Environment</span></div>
-	                    </div>
-	                </li>
-	                <li class="materials">
-	                    <div class="imgdiv">
-	                        <img src="img/cats/materials.jpg" />
-	                    </div>
-	                    <div class="overlay">
-	                        <h3>Revolutionary Materials</h3>
-	                    </div>
-	                    <div class="checkboxes">
-	                        <div data-shortname="nanotech" class="item"><img src="img/box.png" alt="checkbox" /><span>Nanotechnology</span></div>
-	                        <div data-shortname="structures" class="item"><img src="img/box.png" alt="checkbox" /><span>Structures</span></div>
-	                        <div data-shortname="healthsafety" class="item"><img src="img/box.png" alt="checkbox" /><span>Health & Safety</span></div>
-	                        <div data-shortname="eenvironment" class="item"><img src="img/box.png" alt="checkbox" /><span>Energy & Environment</span></div>
-	                        <div data-shortname="electronics" class="item"><img src="img/box.png" alt="checkbox" /><span>Electronics</span></div>
-	                    </div>
-	                </li>
-	        		<li class="healthcare">
-	                    <div class="imgdiv">
-	                        <img src="img/cats/healthcare.jpg" />
-	                    </div>
-	                    <div class="overlay">
-	                        <h3>Healthcare</h3>
-	                    </div>
-	                    <div class="checkboxes">
-	                        <div data-shortname="disease" class="item"><img src="img/box.png" alt="checkbox" /><span>Disease Research</span></div>
-	                        <div data-shortname="diagnostics" class="item"><img src="img/box.png" alt="checkbox" /><span>Diagnostics</span></div>
-	                        <div data-shortname="meddev" class="item"><img src="img/box.png" alt="checkbox" /><span>Medical Devices</span></div>
-	                        <div data-shortname="treatments" class="item"><img src="img/box.png" alt="checkbox" /><span>Treatments</span></div>
-	                        <div data-shortname="globalhealth" class="item"><img src="img/box.png" alt="checkbox" /><span>Global Health</span></div>
-	                    </div>
-	                </li>
-	                <li class="securing">
-	                    <div class="imgdiv">
-	                        <img src="img/cats/securing.jpg" />
-	                    </div>
-	                    <div class="overlay">
-	                        <h3>Securing our Future</h3>
-	                    </div>
-	                    <div class="checkboxes">
-	                        <div data-shortname="weapons" class="item"><img src="img/box.png" alt="checkbox" /><span>Weapons Detection</span></div>
-	                        <div data-shortname="cybersecurity" class="item"><img src="img/box.png" alt="checkbox" /><span>CyberSecurity</span></div>
-	                        <div data-shortname="nuclear" class="item"><img src="img/box.png" alt="checkbox" /><span>Nuclear Non-Proliferation</span></div>
-	                        <div data-shortname="natsec" class="item"><img src="img/box.png" alt="checkbox" /><span>National Security</span></div>
-	                        <div data-shortname="infrastructure" class="item"><img src="img/box.png" alt="checkbox" /><span>Infrastructure</span></div>
-	                        <div data-shortname="natdis" class="item"><img src="img/box.png" alt="checkbox" /><span>Natural Disasters</span></div>
-	                    </div>
-	                </li>
+				<div class="category">
+		        	<h2>Select some things that you like.</h2>
+		        	<ul>
+		        		<li class="transportation">
+		                    <div class="imgdiv">
+		                        <img src="img/cats/transportation.jpg" />
+		                    </div>
+		                    <div class="overlay">
+		                        <h3>Transportation Innovations</h3>
+		                    </div>
+		                    <div class="checkboxes">
+		                        <div data-shortname="vautonomous" class="item"><img src="img/box.png" alt="checkbox" /><span>Autonomous Vehicles</span></div>
+		                        <div data-shortname="invehicle" class="item"><img src="img/box.png" alt="checkbox" /><span>In-Vehicle Technology</span></div>
+		                        <div data-shortname="vefficiency" class="item"><img src="img/box.png" alt="checkbox" /><span>Vehicle Efficiency</span></div>
+		                        <div data-shortname="vsafety" class="item"><img src="img/box.png" alt="checkbox" /><span>Vehicle Safety</span></div>
+		                        <div data-shortname="vpowersources" class="item"><img src="img/box.png" alt="checkbox" /><span>Power Sources</span></div>
+		                    </div>
+		                </li>
+		                <li class="economics">
+		                    <div class="imgdiv">
+		                        <img src="img/cats/economics.jpg" />
+		                    </div>
+		                    <div class="overlay">
+		                        <h3>Economics & Entrepreneurship</h3>
+		                    </div>
+		                    <div class="checkboxes">
+		                        <div data-shortname="startups" class="item"><img src="img/box.png" alt="checkbox" /><span>Startups</span></div>
+		                        <div data-shortname="highrisk" class="item"><img src="img/box.png" alt="checkbox" /><span>High-Risk Research</span></div>
+		                        <div data-shortname="entreco" class="item"><img src="img/box.png" alt="checkbox" /><span>Entrepreneurial Ecosystem</span></div>
+		                        <div data-shortname="ecoimpact" class="item"><img src="img/box.png" alt="checkbox" /><span>Economic Impact</span></div>
+		                    </div>
+		                </li>
+		                <li class="wolverineexperience">
+		                    <div class="imgdiv">
+		                        <img src="img/cats/wolverineexperience.jpg" />
+		                    </div>
+		                    <div class="overlay">
+		                        <h3>Wolverine Experience</h3>
+		                    </div>
+		                    <div class="checkboxes">
+		                        <div data-shortname="handson" class="item"><img src="img/box.png" alt="checkbox" /><span>Hands-On Engineering</span></div>
+		                        <div data-shortname="leadersbest" class="item"><img src="img/box.png" alt="checkbox" /><span>Academic Leaders & Best</span></div>
+		                        <div data-shortname="world" class="item"><img src="img/box.png" alt="checkbox" /><span>Wolverines Around the World</span></div>
+		                        <div data-shortname="innovations" class="item"><img src="img/box.png" alt="checkbox" /><span>Innovations at Michigan</span></div>
+		                        <div data-shortname="stories" class="item"><img src="img/box.png" alt="checkbox" /><span>Victor Stories</span></div>
+		                        <div data-shortname="life" class="item"><img src="img/box.png" alt="checkbox" /><span>Life in Ann Arbor</span></div>
+		                    </div>
+		                </li>
+		                <li class="globalresources">
+		                    <div class="imgdiv">
+		                        <img src="img/cats/globalresources.jpg" />
+		                    </div>
+		                    <div class="overlay">
+		                        <h3>Global Resources</h3>
+		                    </div>
+		                    <div class="checkboxes">
+		                        <div data-shortname="water" class="item"><img src="img/box.png" alt="checkbox" /><span>Water</span></div>
+		                        <div data-shortname="sustain" class="item"><img src="img/box.png" alt="checkbox" /><span>Sustainability</span></div>
+		                        <div data-shortname="eefficiency" class="item"><img src="img/box.png" alt="checkbox" /><span>Energy Efficiency</span></div>
+		                        <div data-shortname="alternativee" class="item"><img src="img/box.png" alt="checkbox" /><span>Alternative Energy</span></div>
+		                        <div data-shortname="environment" class="item"><img src="img/box.png" alt="checkbox" /><span>Environment</span></div>
+		                    </div>
+		                </li>
+		                <li class="materials">
+		                    <div class="imgdiv">
+		                        <img src="img/cats/materials.jpg" />
+		                    </div>
+		                    <div class="overlay">
+		                        <h3>Revolutionary Materials</h3>
+		                    </div>
+		                    <div class="checkboxes">
+		                        <div data-shortname="nanotech" class="item"><img src="img/box.png" alt="checkbox" /><span>Nanotechnology</span></div>
+		                        <div data-shortname="structures" class="item"><img src="img/box.png" alt="checkbox" /><span>Structures</span></div>
+		                        <div data-shortname="healthsafety" class="item"><img src="img/box.png" alt="checkbox" /><span>Health & Safety</span></div>
+		                        <div data-shortname="eenvironment" class="item"><img src="img/box.png" alt="checkbox" /><span>Energy & Environment</span></div>
+		                        <div data-shortname="electronics" class="item"><img src="img/box.png" alt="checkbox" /><span>Electronics</span></div>
+		                    </div>
+		                </li>
+		        		<li class="healthcare">
+		                    <div class="imgdiv">
+		                        <img src="img/cats/healthcare.jpg" />
+		                    </div>
+		                    <div class="overlay">
+		                        <h3>Healthcare</h3>
+		                    </div>
+		                    <div class="checkboxes">
+		                        <div data-shortname="disease" class="item"><img src="img/box.png" alt="checkbox" /><span>Disease Research</span></div>
+		                        <div data-shortname="diagnostics" class="item"><img src="img/box.png" alt="checkbox" /><span>Diagnostics</span></div>
+		                        <div data-shortname="meddev" class="item"><img src="img/box.png" alt="checkbox" /><span>Medical Devices</span></div>
+		                        <div data-shortname="treatments" class="item"><img src="img/box.png" alt="checkbox" /><span>Treatments</span></div>
+		                        <div data-shortname="globalhealth" class="item"><img src="img/box.png" alt="checkbox" /><span>Global Health</span></div>
+		                    </div>
+		                </li>
+		                <li class="securing">
+		                    <div class="imgdiv">
+		                        <img src="img/cats/securing.jpg" />
+		                    </div>
+		                    <div class="overlay">
+		                        <h3>Securing our Future</h3>
+		                    </div>
+		                    <div class="checkboxes">
+		                        <div data-shortname="weapons" class="item"><img src="img/box.png" alt="checkbox" /><span>Weapons Detection</span></div>
+		                        <div data-shortname="cybersecurity" class="item"><img src="img/box.png" alt="checkbox" /><span>CyberSecurity</span></div>
+		                        <div data-shortname="nuclear" class="item"><img src="img/box.png" alt="checkbox" /><span>Nuclear Non-Proliferation</span></div>
+		                        <div data-shortname="natsec" class="item"><img src="img/box.png" alt="checkbox" /><span>National Security</span></div>
+		                        <div data-shortname="infrastructure" class="item"><img src="img/box.png" alt="checkbox" /><span>Infrastructure</span></div>
+		                        <div data-shortname="natdis" class="item"><img src="img/box.png" alt="checkbox" /><span>Natural Disasters</span></div>
+		                    </div>
+		                </li>
         		
-	        	</ul>
-	        	<div class="next"><a class="next" href="#">Submit</a><img src="img/whiteblue.gif" class="wheel" /></div>
-
+		        	</ul>
+		        	<div class="next" style="overflow:hidden"><a class="next catnext" href="#">Submit</a><img src="img/whiteblue.gif" class="wheel" /></div>
+				</div>
+				
+				<div class="accountinfo" style="display:none">
+		        	<h2>Set your account information.</h2>
+					<ul>
+						<li><label for="acc_name">Name:</label><input id="acc_name" type="text"></li>
+						<li><label for="acc_email">Email:</label><input id="acc_email" type="text"></li>
+					</ul>
+		        	<div class="next" style="overflow:hidden"><a class="next accnext" href="#">Submit</a><img src="img/whiteblue.gif" class="wheel" /></div>
+				</div>
 	        </div>
 			
 			
@@ -441,7 +456,7 @@
 						<input type="hidden" id="w" name="w" />
 						<input type="hidden" id="h" name="h" />
 						<input type="hidden" id="avatarurl" name="avatarurl" value=""/>
-			        	<div class="next"  style="margin-top:20px"><a class="next" href="#" onclick="document.getElementById('cropform').submit()">Submit Avatar</a></div>
+			        	<div class="next"  style="margin-top:20px; overflow:hidden"><a class="next" href="#" onclick="document.getElementById('cropform').submit()">Submit Avatar</a></div>
 					</form>
 				</div>
 			</div>
@@ -486,7 +501,7 @@
 					}
 					
 				});
-				$("a.next").on("click", function (e){
+				$("a.catnext").on("click", function (e){
 					e.preventDefault();
 					// console.log($(".wheel")[0])
 					$(".wheel").toggle();
@@ -504,10 +519,26 @@
 						data: { id: <?= $user?>, type: "newcats", cats: query }
 					}).done(function( msg ) {
 						console.log("Message: ", msg);
-						if (msg == "added cats") window.location.assign("index.php");
+						//if (msg == "added cats") window.location.assign("index.php");
+						$(".wheel").toggle();
+						if (msg == "added cats") alert("Update Successes!");
 					});
 					console.log(query);
 				});
+				
+				// tabs switch
+				$('.cattab').on('click',function(){
+					$('.tabs ul li').removeClass();
+					$(this).addClass('selected');
+					$('.category').fadeIn('slow');
+					$('.accountinfo').fadeOut('slow');
+				})
+				$('.acctab').on('click',function(){
+					$('.tabs ul li').removeClass();
+					$(this).addClass('selected');
+					$('.category').fadeOut('slow');
+					$('.accountinfo').fadeIn('slow');
+				})
 				
 				//upload avatar images
 				
