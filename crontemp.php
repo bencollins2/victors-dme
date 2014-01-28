ab9523fae2ec95b5098fcfc12bb36b60adc4ff09
leonatefm <chengqizhufm@gmail.com>
2014-01-28 16:51:43 -0500

    crontemp completed

--- crontemp.php
+++ crontemp.php
@@ -19,8 +19,11 @@
 
     	$story_json = file_get_contents($json_url);
     	$story_data = json_decode($story_json, TRUE);
+		
+		$count = 0;
 
-    	// if($story_data["Include in Campaign DME"] == "True") {
+    	if($story_data["Include in Campaign DME"] == "True") {
+				$count = $count + 1;
     			$title = ($story_data["Short Title"] != "") ? $story_data["Short Title"] : $story_data["Title"];
 				$byline = $story_data["Author"];
 				$description = $story_data["Subtitle"];
@@ -28,8 +31,13 @@
 				$img_large = $story_data["Campaign DME Image"];
 				$img_slice = $story_data["Campaign DME Image Slice"];
 				$html = $story_data["Body content"];
-    	// }
+				
+				$query = "INSERT INTO `features` (`id`, `title`, `byline`, `description`, `longdesc`, `img_large`, `tags`, `html`, `options`, `customStyle`, `story_images`, `pullquotes`, `weight`, `public`, `link`, `titletop`, `pubdate`) VALUES (NULL, '$title', '$byline', '$description', '$longdesc', '$img_large', NULL, '$html', NULL, NULL, NULL, NULL, NULL, '0', NULL, '1', NULL)";
+				
+    	}
     }
+	
+	echo "Databased Updated! $count stories have been added.";
 
 
 
